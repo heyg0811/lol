beae0d79364adcf3eb41582f7174ea2bfdcc2b92
heyg0811 <heyg0811@gmail.com>
2014-12-26 21:39:00 +0000

    fix the layout

--- fuel/app/views/data/character/detail.php
+++ fuel/app/views/data/character/detail.php
@@ -1,23 +1,25 @@
 <div class="row">
-  <h2 class="text-center"><?php echo $character['name']; ?></h2>
+  <h1 class="char-name"><?php echo $character['name']; ?></h1>
   <div class="char-card">
     <?php echo Asset::img('character/card/'.$character['path'].'.png', array('class'=>'img-responsive')); ?>
   </div>
 	
-	<h3 class="bs-callout bs-callout-info">スキル</h3>
+	<h2 class="bs-callout bs-callout-info">スキル</h2>
 	<?php foreach ($character['skill'] as $skill): ?>
-  	  <h4>- <?php echo $skill['name']; ?></h4>
-      <div class="row">
-        <div class="col-sm-7">
-          <p><?php echo $skill['description']; ?></p>
-        </div>
-        <div class="col-sm-5">
-          <dl class="dl-horizontal">
-            <dt>攻撃力</dt>
-            <dd><?php echo $skill['atack']; ?></dd>
-            <dt>クールダウン</dt>
-            <dd><?php echo $skill['cool']; ?>秒</dd>
-          </dl>
+  	  <h3>- <?php echo $skill['name']; ?></h3>
+      <div class="row char-skill">
+        <div class="col-xs-12">
+          <div class="col-sm-7">
+            <p><?php echo $skill['description']; ?></p>
+          </div>
+          <div class="col-sm-5">
+            <dl class="dl-horizontal">
+              <dt>攻撃力</dt>
+              <dd><?php echo $skill['atack']; ?></dd>
+              <dt>クールダウン</dt>
+              <dd><?php echo $skill['cool']; ?>秒</dd>
+            </dl>
+          </div>
         </div>
       </div>
       <div class="iframe-content text-center">
